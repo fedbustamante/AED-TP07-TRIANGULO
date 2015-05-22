9ee34a78be83451e1081eb4cb4b1f58ccda82eeb
ChamHerz <adrielchambers@gmail.com>
2015-05-22 11:15:52 -0300

    Iniciando TP7

---
+++ TP07-TRI.cpp
@@ -0,0 +1,26 @@
+#include <cstdlib>
+#include <iostream>
+
+/* Trabajp Practico N7 
+
+REQUISITO:
+Ingresado 3 medidas por el Usuario verificar si puede formar o no un Triangulo.
+
+Determinar que tipo de triangulo puede ser: Equilatero, Isoseles o Escaleno.
+
+MATERIA:
+Algoritmo y Estructura de Datos
+
+INTEGRANTES:
+Martin AGUEL
+Adriel CHAMBI
+Federico BUSTAMANTE
+Augusto SCHMID
+*/
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<"Iniciando";
+}
